79400edbf14b5eb42e0ba86dcaa422f516066333
Nourdine <noureddinebentaher20@gmail.com>
2024-08-21 19:58:22 +0100

    added reports component,report-details component

---
+++ adminFront/src/app/reports-details/reports-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reports-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './reports-details.component.html',
+  styleUrl: './reports-details.component.css'
+})
+export class ReportsDetailsComponent {
+
+}
