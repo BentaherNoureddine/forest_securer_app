c90b97942023ee4cd5d80802c8976096bb92ff3e
Nourdine <noureddinebentaher20@gmail.com>
2024-08-22 13:16:04 +0100

    added report service and report class

---
+++ adminFront/src/app/models/report.ts
@@ -0,0 +1,11 @@
+import {Category} from "./Category";
+
+export class Report {
+
+   id?: number  ;
+   category?: Category;
+   description?: string;
+   reporterId? : string;
+   imagePath?: string;
+   location? : string;
+}
